1090470bf91752256fc8d8da1a7825903fa1ebbe
Nelson Robles <nelsonroblesv@gmail.com>
2024-12-17 17:26:25 -0600

    V1.2.9. Add Product View Page

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -4,10 +4,28 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
     protected static ?string $title = 'Registrar Producto';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Producto registrado')
+            ->body('Se ha registrado un nuevo Producto de forma exitosa.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
+
 }
--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProduct extends EditRecord
@@ -11,11 +12,28 @@ class EditProduct extends EditRecord
     protected static string $resource = ProductResource::class;
     protected static ?string $title = 'Editar Producto';
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Cambios realizados')
+            ->body('Se ha actualizado el registro correctamente.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
             Actions\DeleteAction::make()
-            ->label('Borrar Producto'),
+                ->label('Borrar'),
         ];
     }
+
 }
