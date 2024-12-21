4c2c6bb23f75113d9ee58172ba0be49273160340
Nelson Robles <nelsonroblesv@gmail.com>
2024-12-20 18:00:50 -0600

    Remove OrderObserver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Payment::observe(PaymentObserver::class);
-        Order::observe(OrderObserver::class);
 
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
