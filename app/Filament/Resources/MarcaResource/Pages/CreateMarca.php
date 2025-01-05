0452629470342744a9f4922119c4aa8607b9a852
Nelson Robles <nelsonroblesv@gmail.com>
2025-01-05 09:14:30 -0600

    Update. Enhance CreateMarca page with custom title and success notification for improved user feedback.

--- app/Filament/Resources/MarcaResource/Pages/CreateMarca.php
+++ app/Filament/Resources/MarcaResource/Pages/CreateMarca.php
@@ -4,9 +4,28 @@ namespace App\Filament\Resources\MarcaResource\Pages;
 
 use App\Filament\Resources\MarcaResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMarca extends CreateRecord
 {
     protected static string $resource = MarcaResource::class;
+    protected static ?string $title = 'Nueva Marca';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Marca registrada')
+            ->body('Se ha registrado una nueva Marca de forma exitosa.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
+
 }
