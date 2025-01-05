5272157b708c9ea06f49f2e4259eefc308b9a051
Nelson Robles <nelsonroblesv@gmail.com>
2025-01-05 08:55:24 -0600

    Add Marca resource with CRUD functionality and migration for marcas table

---
+++ app/Filament/Resources/MarcaResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MarcaResource\Pages;
+use App\Filament\Resources\MarcaResource\RelationManagers;
+use App\Models\Marca;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MarcaResource extends Resource
+{
+    protected static ?string $model = Marca::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Informacion de la marca')
+                    ->schema([
+                        TextInput::make('name')->label('Nombre')->required()->maxLength(255),
+                        TextInput::make('description')->label('Descripcion')->maxLength(255),
+                        FileUpload::make('logo')
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('logo')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMarcas::route('/'),
+            'create' => Pages\CreateMarca::route('/create'),
+            'edit' => Pages\EditMarca::route('/{record}/edit'),
+        ];
+    }
+}
