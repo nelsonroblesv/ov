d0a0264a11c7424af3dfd2e0c33946d25a948927
Nelson Robles <nelsonroblesv@gmail.com>
2025-02-05 17:00:46 -0600

    Update. Enhance EditServices page with custom success notification and redirect URL.

--- app/Filament/Resources/ServicesResource/Pages/EditServices.php
+++ app/Filament/Resources/ServicesResource/Pages/EditServices.php
@@ -4,16 +4,37 @@ namespace App\Filament\Resources\ServicesResource\Pages;
 
 use App\Filament\Resources\ServicesResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditServices extends EditRecord
 {
     protected static string $resource = ServicesResource::class;
 
+    protected static ?string $title = 'Editar Servicio';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Cambios realizados')
+            ->body('Se ha actualizado el registro correctamente.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->label('Borrar'),
         ];
     }
+
 }
