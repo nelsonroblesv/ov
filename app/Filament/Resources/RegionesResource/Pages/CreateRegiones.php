f83c6c1153edb3a4d89149b08e9f152f69a14244
Nelson Robles <nelsonroblesv@gmail.com>
2025-02-07 21:19:08 -0600

    Add success notification and redirect for creating Regiones

--- app/Filament/Resources/RegionesResource/Pages/CreateRegiones.php
+++ app/Filament/Resources/RegionesResource/Pages/CreateRegiones.php
@@ -4,9 +4,28 @@ namespace App\Filament\Resources\RegionesResource\Pages;
 
 use App\Filament\Resources\RegionesResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRegiones extends CreateRecord
 {
     protected static string $resource = RegionesResource::class;
+    
+    protected static ?string $title = 'Registrar Región';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Región registrada')
+            ->body('Se ha registrado una nueva Región de forma exitosa.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
 }
