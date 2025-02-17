fc4883a8acd620fe76de5eee076d9d56abeb4009
Nelson Robles <nelsonroblesv@gmail.com>
2025-02-16 20:18:09 -0600

    Enhance Prospectos creation and editing pages; add custom notifications and redirect URLs

--- app/Filament/App/Resources/ProspectosResource/Pages/CreateProspectos.php
+++ app/Filament/App/Resources/ProspectosResource/Pages/CreateProspectos.php
@@ -4,9 +4,28 @@ namespace App\Filament\App\Resources\ProspectosResource\Pages;
 
 use App\Filament\App\Resources\ProspectosResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProspectos extends CreateRecord
 {
     protected static string $resource = ProspectosResource::class;
+    protected static ?string $title = 'Registrar Prospecto';
+    
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Prospecto registrado')
+            ->body('Se ha registrado un nuevo Prospecto de forma exitosa.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
 }
--- app/Filament/App/Resources/ProspectosResource/Pages/EditProspectos.php
+++ app/Filament/App/Resources/ProspectosResource/Pages/EditProspectos.php
@@ -3,17 +3,39 @@
 namespace App\Filament\App\Resources\ProspectosResource\Pages;
 
 use App\Filament\App\Resources\ProspectosResource;
+use Cheesegrits\FilamentGoogleMaps\Concerns\InteractsWithMaps;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProspectos extends EditRecord
 {
     protected static string $resource = ProspectosResource::class;
+    use InteractsWithMaps;
+
+    protected static ?string $title = 'Editar Prospecto';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Cambios realizados')
+            ->body('Se ha actualizado el registro correctamente.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->label('Borrar'),
         ];
     }
 }
