d936e295b824dc3b7327059188758c834629e83e
Nelson Robles <nelsonroblesv@gmail.com>
2025-02-18 21:46:17 -0600

    Add notifications and redirect URLs to Create and Edit CustomerUser pages

--- app/Filament/App/Resources/CustomerUserResource/Pages/CreateCustomerUser.php
+++ app/Filament/App/Resources/CustomerUserResource/Pages/CreateCustomerUser.php
@@ -4,9 +4,28 @@ namespace App\Filament\App\Resources\CustomerUserResource\Pages;
 
 use App\Filament\App\Resources\CustomerUserResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCustomerUser extends CreateRecord
 {
     protected static string $resource = CustomerUserResource::class;
+    protected static ?string $title = 'Registrar Cliente';
+    
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Cliente registrado')
+            ->body('Se ha registrado un nuevo Cliente de forma exitosa.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
 }
--- app/Filament/App/Resources/CustomerUserResource/Pages/EditCustomerUser.php
+++ app/Filament/App/Resources/CustomerUserResource/Pages/EditCustomerUser.php
@@ -3,17 +3,40 @@
 namespace App\Filament\App\Resources\CustomerUserResource\Pages;
 
 use App\Filament\App\Resources\CustomerUserResource;
+use Cheesegrits\FilamentGoogleMaps\Concerns\InteractsWithMaps;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCustomerUser extends EditRecord
 {
     protected static string $resource = CustomerUserResource::class;
+    use InteractsWithMaps;
+
+    protected static ?string $title = 'Editar Cliente';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Cambios realizados')
+            ->body('Se ha actualizado el registro correctamente.')
+            ->icon('heroicon-o-check')
+            ->iconColor('success')
+            ->color('success');
+    }
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->label('Borrar'),
         ];
     }
 }
+
