dadf72699d6fe0086b6e7bf3f6496b94a0c04225
Nelson Robles <nelsonroblesv@gmail.com>
2025-03-04 17:25:23 -0600

    Enhance AsignarTipoSemanaResource: update navigation icon, group, label, breadcrumb, and sort order for improved organization

--- app/Filament/Resources/AsignarTipoSemanaResource.php
+++ app/Filament/Resources/AsignarTipoSemanaResource.php
@@ -19,7 +19,11 @@ class AsignarTipoSemanaResource extends Resource
 {
     protected static ?string $model = AsignarTipoSemana::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-date-range';
+    protected static ?string $navigationGroup = 'Administrar';
+    protected static ?string $navigationLabel = 'Tipo Semana';
+    protected static ?string $breadcrumb = 'Bitacora';
+    protected static ?int $navigationSort = 4;
 
     public static function form(Form $form): Form
     {
