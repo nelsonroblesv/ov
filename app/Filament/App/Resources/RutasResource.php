6acab70e97995aab2fceef207ee38f9b71844920
Nelson Robles <nelsonroblesv@gmail.com>
2025-03-19 20:09:21 -0600

    Add Prospeccion Modal form in RutasResource. Change notification color, message, icon

--- app/Filament/App/Resources/RutasResource.php
+++ app/Filament/App/Resources/RutasResource.php
@@ -10,6 +10,7 @@ use App\Models\BitacoraCustomers;
 use App\Models\Customer;
 use App\Models\Regiones;
 use App\Models\Rutas;
+use App\Models\Services;
 use App\Models\Zonas;
 use Carbon\Carbon;
 use Cheesegrits\FilamentGoogleMaps\Fields\Map;
@@ -268,7 +269,9 @@ class RutasResource extends Resource
 
                         Notification::make()
                             ->title('Cliente registrado')
-                            ->success()
+                            ->body('El cliente ha sido registrado con éxito. Recuerda completar la información adicional en otro momento.')
+                            ->icon('heroicon-o-user-plus')
+                            ->color('success')
                             ->send();
                     }),
 
@@ -279,27 +282,226 @@ class RutasResource extends Resource
                     ->icon('heroicon-m-magnifying-glass')
                     ->color('warning')
                     ->form([
-                        Section::make('Datos personales')->schema([
+                        Section::make('Información Personal')
+                            ->description('Completa los campos con la información requerida.')
+                            ->schema([
 
-                            Hidden::make('user_id')->default(fn() => auth()->id()),
+                                Hidden::make('user_id')->default(fn() => auth()->id()),
 
-                            TextInput::make('name')
-                                ->label('Nombre completo')
-                                ->required()
-                                ->maxLength(255)
-                                ->unique(ignoreRecord: true)
-                                ->suffixIcon('heroicon-m-user'),
+                                ToggleButtons::make('tipo_cliente')
+                                    ->label('Tipo de Registro')
+                                    ->required()
+                                    ->inline()
+                                    ->options([
+                                        'PO' => 'Posible',
+                                        'PR' => 'Prospecto',
+                                    ])
+                                    ->default('PO')
+                                    ->colors([
+                                        'PO' => 'danger',
+                                        'PR' => 'warning'
+                                    ])
+                                    ->icons([
+                                        'PO' => 'heroicon-o-map',
+                                        'PR' => 'heroicon-o-star'
+                                    ]),
+
+                                TextInput::make('name')
+                                    ->label('Nombre completo')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->unique(table: Customer::class, column: 'name')
+                                    ->suffixIcon('heroicon-m-user'),
+
+                                Select::make('services')
+                                    ->label('Servicios')
+                                    ->placeholder('Selecciona uno o mas servicios')
+                                    ->multiple()
+                                    ->preload()
+                                    ->searchable()
+                                    ->options(Services::pluck('name', 'name'))
+                                    ->suffixIcon('heroicon-m-sparkles'),
+
+                                Select::make('simbolo')
+                                    ->label('Simbologia')
+                                    ->options([
+                                        'SB' => 'Salón de Belleza',
+                                        'BB' => 'Barbería',
+                                        'UN' => 'Salón de Uñas',
+                                        'OS' => 'OSBERTH',
+                                        'CR' => 'Cliente Pedido Rechazado',
+                                        'UB' => 'Ubicación en Grupo',
+                                        'NC' => 'Ya no compran'
+                                    ]),
+
+                                Toggle::make('reventa')->label('Ya maneja Reventa')->default(false)
+                                    ->onIcon('heroicon-m-check')
+                                    ->offIcon('heroicon-m-x-mark')
+                                    ->onColor('success')
+                                    ->offColor('danger')
+                                    ->inline(true),
+
+                            ])->columns(2),
+
+                        Section::make('Datos de contacto')
+                            ->description('Ingresa la información de contacto.')
+                            ->schema([
+                                TextInput::make('email')
+                                    ->label('Correo electrónico')
+                                    ->email()
+                                    ->unique(table: Customer::class, column: 'email')
+                                    ->maxLength(255)
+                                    ->suffixIcon('heroicon-m-at-symbol'),
+
+                                TextInput::make('phone')
+                                    ->label('Teléfono')
+                                    ->tel()
+                                    ->unique(table: Customer::class, column: 'phone')
+                                    ->maxLength(50)
+                                    ->suffixIcon('heroicon-m-phone'),
+
+                                FileUpload::make('fachada')
+                                    ->label('Foto de fachada')
+                                    ->placeholder('Tomar foto o cargar desde galeria')
+                                    ->image()
+                                    ->imageEditor()
+                                    ->directory('prospectos-images')
+                                    ->columnSpanFull()
+                            ])->columns(2),
+
+                        Section::make('Ubicación')
+                            ->description('Ingresa la ubicación del cliente')
+                            ->schema([
+                                TextInput::make('full_address')
+                                    ->label('Dirección')
+                                    ->helperText('Calle, Núm. Ext., Núm. Int., Colonia, Intersecciones')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->suffixIcon('heroicon-m-map')
+                                    ->columnSpanFull(),
+
+                                Map::make('location')
+                                    ->mapControls([
+                                        'mapTypeControl'    => true,
+                                        'scaleControl'      => true,
+                                        'streetViewControl' => false,
+                                        'rotateControl'     => true,
+                                        'fullscreenControl' => true,
+                                        'searchBoxControl'  => false,
+                                        'zoomControl'       => true,
+                                    ])
+                                    ->defaultZoom(15)
+                                    ->autocomplete('full_address')
+                                    ->autocompleteReverse(true)
+                                    ->reverseGeocode([
+                                        'street' => '%n %S',
+                                        'city' => '%L',
+                                        'state' => '%A1',
+                                        'zip' => '%z',
+                                    ])
+
+                                    ->layers([
+                                        'https://app.osberthvalle.com/storage/maps/zonas_ovalle.kml',
+                                    ])
+
+                                    ->debug()
+                                    ->draggable()
+
+                                    ->geolocate()
+                                    ->geolocateLabel('Obtener mi Ubicacion')
+                                    ->geolocateOnLoad(true, false)
+
+                                    ->defaultLocation(fn($record) => [
+                                        $record->latitude ?? 19.8386943,
+                                        $record->longitude ?? -90.4982317,
+                                    ])
+                                    ->columnSpanFull()->reactive()
+                                    ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                        $set('latitude', $state['lat']);
+                                        $set('longitude', $state['lng']);
+                                    }),
+
+                                Hidden::make('latitude')
+                                    ->label('Latitud')
+                                    ->helperText('Formato: 20.1845751')
+                                    // ->unique(ignoreRecord: true)
+                                    ->reactive()
+                                    ->dehydrated()
+                                    ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                        $set('location', [
+                                            'lat' => floatVal($state),
+                                            'lng' => floatVal($get('longitude')),
+                                        ]);
+                                    })->lazy(),
+
+                                Hidden::make('longitude')
+                                    ->label('Longitud')
+                                    ->helperText('Formato: 20.1845751')
+                                    //->unique(ignoreRecord: true)
+                                    ->dehydrated()
+                                    ->reactive()
+                                    ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                        $set('location', [
+                                            'lat' => floatval($get('latitude')),
+                                            'lng' => floatVal($state),
+                                        ]);
+                                    })->lazy(),
 
 
-                            TextInput::make('email')
-                                ->label('Correo electrónico')
-                                ->email()
-                                ->required()
-                                ->unique(ignoreRecord: true)
-                                ->maxLength(255)
-                                ->suffixIcon('heroicon-m-at-symbol'),
-                        ])
-                    ]),
+                                Select::make('regiones_id')
+                                    ->label('Región')
+                                    ->required()
+                                    ->options(
+                                        fn() =>
+                                        Regiones::whereIn('id', function ($query) {
+                                            $query->select('regiones_id')
+                                                ->from('zonas')
+                                                ->where('user_id', auth()->id());
+                                        })->pluck('name', 'id')
+                                    )
+                                    ->reactive(),
+
+                                Select::make('zonas_id')
+                                    ->label('Zona')
+                                    ->placeholder('Selecciona una zona')
+                                    ->required()
+                                    ->searchable()
+                                    ->options(
+                                        fn(callable $get) =>
+                                        Zonas::where('regiones_id', $get('regiones_id'))
+                                            ->whereIn('id', function ($query) {
+                                                $query->select('id')
+                                                    ->from('zonas')
+                                                    ->where('user_id', auth()->id());
+                                            })
+                                            ->pluck('nombre_zona', 'id')
+                                    )
+                                    ->reactive()
+                                    ->disabled(fn(callable $get) => empty($get('regiones_id'))),
+                            ])->columns(2),
+
+                        Section::make('Notas Generales')
+                            ->description('Despliega para agregar notas adicionales')
+                            ->collapsed()
+                            ->schema([
+                                MarkdownEditor::make('extra')
+                                    ->label('Extra')
+                                    ->nullable()
+                                    ->columnSpanFull()
+                            ])
+                            ->columnSpanFull(),
+                    ])
+                    ->action(function (array $data) {
+                        $data['created_at'] = Carbon::now()->setTimezone('America/Merida');
+                        Customer::create($data);
+
+                        Notification::make()
+                            ->title('Prospección registrada')
+                            ->body('Se ha registrado la Prospección con éxito. Recuerda completar la información adicional en otro momento.')
+                            ->icon('heroicon-o-magnifying-glass')
+                            ->color('success')
+                            ->send();
+                    }),
             ])
             ->columns([
                 TextColumn::make('sort')->label('#')->sortable(),
