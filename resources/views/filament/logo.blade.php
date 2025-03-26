3430b80ea75bd1f7f1532f50cfb650dca8d0e034
Nelson Robles <nelsonroblesv@gmail.com>
2025-03-26 16:37:09 -0600

    Update logo size in Filament view: reduce width for improved layout

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,4 +1,3 @@
 <div class="flex items-center space-x-4">
-    <img src="{{ asset('images/logo_ovalleB.png') }}" alt="OValle" width="70" style="margin-bottom:20px
-    ">    
+    <img src="{{ asset('images/logo_ovalleB.png') }}" alt="OValle" width="45" style="">   
 </div>
