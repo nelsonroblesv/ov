7597982877f89a530f0bfee8b2a78f91118de60b
Nelson Robles <nelsonroblesv@gmail.com>
2025-03-26 16:29:45 -0600

    Update logo styling in Filament view: add bottom margin to improve layout

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,3 +1,4 @@
 <div class="flex items-center space-x-4">
-    <img src="{{ asset('images/logo_ovalleB.png') }}" alt="OValle" width="70">    
+    <img src="{{ asset('images/logo_ovalleB.png') }}" alt="OValle" width="70" style="margin-bottom:20px
+    ">    
 </div>
