3ea43824835f861579c55345c17a741cfd622259
Nelson Robles <nelsonroblesv@gmail.com>
2025-03-28 07:44:09 -0600

    Add timestamp mutation for created_at field in CreatePaqueteGuia page

--- app/Filament/Resources/PaqueteGuiaResource/Pages/CreatePaqueteGuia.php
+++ app/Filament/Resources/PaqueteGuiaResource/Pages/CreatePaqueteGuia.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Resources\PaqueteGuiaResource\Pages;
 
 use App\Filament\Resources\PaqueteGuiaResource;
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePaqueteGuia extends CreateRecord
 {
     protected static string $resource = PaqueteGuiaResource::class;
+
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_at'] = Carbon::now()->setTimezone('America/Merida')->format('Y-m-d H:i:s');
+        return $data;
+    }
 }
