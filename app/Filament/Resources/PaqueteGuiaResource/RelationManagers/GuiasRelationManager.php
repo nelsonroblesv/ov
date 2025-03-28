966a8e07ab6d5ff90b26b2c8b0ac41df979985cb
Nelson Robles <nelsonroblesv@gmail.com>
2025-03-28 08:29:31 -0600

    Refactor GuiasRelationManager to enhance form structure and improve table columns with additional features

--- app/Filament/Resources/PaqueteGuiaResource/RelationManagers/GuiasRelationManager.php
+++ app/Filament/Resources/PaqueteGuiaResource/RelationManagers/GuiasRelationManager.php
@@ -3,24 +3,37 @@
 namespace App\Filament\Resources\PaqueteGuiaResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\Summarizers\Count;
+use Filament\Tables\Columns\Summarizers\Sum;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class GuiasRelationManager extends RelationManager
-{
-    protected static string $relationship = 'Guias';
+{   
+    protected static string $relationship = 'guias';
 
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('numero_guia')
-                    ->required()
-                    ->maxLength(255),
+               Section::make('Datos de la Guía')
+                    ->schema([
+                        TextInput::make('numero_guia')
+                            ->required()
+                            ->maxLength(255),
+                    ])
+                    ->columns(1)
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -29,8 +42,21 @@ class GuiasRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('numero_guia')
             ->columns([
-                Tables\Columns\TextColumn::make('numero_guia'),
+                TextColumn::make('numero_guia')->searchable()->sortable(),
+                IconColumn::make('revisado')
+                    ->boolean()
+                    ->label('Revisado'),
+
+                TextColumn::make('estado')->badge()
+                    ->colors([
+                        'secondary' => 'Pendiente',
+                        'danger' => 'Incidencia',
+                        'success' => 'Revisado',
+                    ])
+                    ->sortable()
+                    ->summarize(Sum::make()->label('Cantidad de Paquetes')),
             ])
+            
             ->filters([
                 //
             ])
