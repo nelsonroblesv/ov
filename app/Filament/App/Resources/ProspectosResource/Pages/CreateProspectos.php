28e7ffe85553c6dbab58d2df0e1bcfd833b3fb61
Nelson Robles <nelsonroblesv@gmail.com>
2025-04-06 15:18:12 -0600

    Add created_at timestamp and route management logic in CreateProspectos. Enhance afterCreate to insert data into GestionRutas with user and region details.

--- app/Filament/App/Resources/ProspectosResource/Pages/CreateProspectos.php
+++ app/Filament/App/Resources/ProspectosResource/Pages/CreateProspectos.php
@@ -3,7 +3,9 @@
 namespace App\Filament\App\Resources\ProspectosResource\Pages;
 
 use App\Filament\App\Resources\ProspectosResource;
+use App\Models\GestionRutas;
 use App\Models\User;
+use App\Models\Zonas;
 use Carbon\Carbon;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
@@ -31,28 +33,53 @@ class CreateProspectos extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        $data['created_at'] = Carbon::now()->setTimezone('America/Merida');
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $customer = $this->record;
+        $regionesId = $customer->regiones_id;
+        $zonasId = $customer->zonas_id;
+
+        $zonas = Zonas::find($zonasId);
+        $tipoSemana = $zonas->tipo_semana;
+        $diaSemana = $zonas->dia_zona;
+
+        if ($customer) {
+            GestionRutas::insert([
+                'user_id'     => auth()->id(),
+                'region_id'   => $regionesId,
+                'zona_id'     => $zonasId,
+                'tipo_semana' => $tipoSemana,
+                'dia_semana'  => $diaSemana,
+                'customer_id' => $customer->id,
+                'created_at'  => now('America/Merida'),
+                'updated_at'  => now('America/Merida'),
+            ]);
+        }
+
         $recipient = User::where('role', 'Administrador')->get();
-        $username =  User::find($data['user_id'])->name;
-        $tipo_cliente = $data['tipo_cliente'];
-        //$data['name'] = ucwords(strtolower($data['name'])); // Convierte a minúsculas y luego pone mayúsculas iniciales
+        $username = auth()->user()?->name ?? 'Usuario';
+        $tipoCliente = $customer->tipo_cliente;
         $tipos = [
             'PV' => 'Punto de Venta',
             'RD' => 'Red',
             'BK' => 'Black',
             'SL' => 'Silver',
             'PO' => 'Posible',
-            'PR' => 'Prospecto'        
+            'PR' => 'Prospecto'
         ];
-        $cliente = $tipos[$tipo_cliente];
-        $data['created_at'] = Carbon::now()->setTimezone('America/Merida');
+        $clienteTipoTexto = $tipos[$tipoCliente] ?? 'Desconocido';
 
         Notification::make()
             ->title('Nuevo Cliente Registrado')
-            ->body("El usuario ". $username." ha registrado a {$data['name']} como {$cliente}.")
+            ->body("El usuario {$username} ha registrado a {$customer->name} como nuevo Cliente {$clienteTipoTexto}. Se ha agregado a la Ruta del usuario.")
             ->icon('heroicon-o-information-circle')
             ->iconColor('info')
             ->color('info')
             ->sendToDatabase($recipient);
-        return $data;
     }
 }
