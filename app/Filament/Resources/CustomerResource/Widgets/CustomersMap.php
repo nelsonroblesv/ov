fa6f46843d5013c249cb08816588eb951ee842de
Nelson Robles <nelsonroblesv@gmail.com>
2025-04-08 05:18:22 -0600

    Refactor getTableActions in CustomersMap to uncomment delete action and improve code readability

--- app/Filament/Resources/CustomerResource/Widgets/CustomersMap.php
+++ app/Filament/Resources/CustomerResource/Widgets/CustomersMap.php
@@ -157,7 +157,7 @@ class CustomersMap extends MapTableWidget
 
 				EditAction::make('edit')
 					->url(fn(Customer $record): string => CustomerResource::getUrl('edit', ['record' => $record])),
-				/*
+				
 					ActionsDeleteAction::make()
                         ->successNotification(
                             Notification::make()
@@ -171,7 +171,7 @@ class CustomersMap extends MapTableWidget
                         ->modalHeading('Borrar Cliente')
                         ->modalDescription('Estas seguro que deseas eliminar este Cliente? Esta acción no se puede deshacer.')
                         ->modalSubmitActionLabel('Si, eliminar'),
-		*/
+		
 			])
 		];
 		//GoToAction::make()->zoom(14)->label('Ver en Mapa')->color('success'),
