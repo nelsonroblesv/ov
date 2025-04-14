b076e490bae7099e32f383a826654264afb31ee9
Nelson Robles <nelsonroblesv@gmail.com>
2025-04-14 09:33:12 -0600

    refactor: Remove unused UbicacionUsuarioController methods and clean up code

--- app/Http/Controllers/UbicacionUsuarioController.php
+++ app/Http/Controllers/UbicacionUsuarioController.php
@@ -7,26 +7,3 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon as SupportCarbon;
 use Illuminate\Support\Facades\Log;
-
-class UbicacionUsuarioController extends Controller
-{
-    public function store(Request $request)
-    {
-        Log::info('Usuario autenticado:', ['user_id' => auth()->id()]);
-
-        $request->validate([
-            'latitud' => 'required|numeric',
-            'longitud' => 'required|numeric',
-        ]);
-
-        UbicacionUsuario::create([
-            'user_id' => auth()->id(),
-            'latitud' => $request->lat,
-            'longitud' => $request->lng,
-            'created_at' => Carbon::now()->setTimezone('America/Merida'),
-
-        ]);
-
-        return response()->json(['status' => 'success']);
-    }
-}
