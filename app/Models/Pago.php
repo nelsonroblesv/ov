154bd004aceca95ec3d2044b34b2c716c922ae05
Nelson Robles <nelsonroblesv@gmail.com>
2025-04-15 16:27:47 -0600

    feat: Add 'periodo', 'semana', and 'tipo_semana' fields to Pago model and migration

--- app/Models/Pago.php
+++ app/Models/Pago.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pago extends Model
 {
-    protected $fillable = ['cobranza_id', 'monto', 'comprobante', 'tipo_pago', 'created_at'];
+    protected $fillable = ['cobranza_id', 'monto', 'comprobante', 'tipo_pago', 
+            'created_at',  'updated_at', 'periodo', 'semana', 'tipo_semana'];
 
     public function cobranza(): BelongsTo
     {
