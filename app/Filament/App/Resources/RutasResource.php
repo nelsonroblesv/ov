bb0945222bbda2819af03eca9cacd9c8c5c4dd28
Nelson Robles <nelsonroblesv@gmail.com>
2025-04-18 18:49:52 -0600

    feat: Improve payment registration description for clarity in RutasResource

--- app/Filament/App/Resources/RutasResource.php
+++ app/Filament/App/Resources/RutasResource.php
@@ -862,8 +862,8 @@ class RutasResource extends Resource
 
                                 Section::make('Registrar Pago')
                                     ->description('IMPORTANTE. Haz click o toca aqui para desplegar la información requerida, 
-                                            únicamente si el cliente registra un pago durante la visita, el monto no puede quedar vacío. Si
-                                            el cliente no realiza un pago, no es necesario llenar esta información.')
+                                            únicamente si el cliente registra un pago durante la visita, el monto no puede quedar vacío. 
+                                            Si el cliente no realiza un pago, no es necesario llenar la información del pago en este momento.')
                                     ->visible(fn($get) => $get('tipo_visita') === 'EN' || $get('tipo_visita') === 'RE')
                                     ->schema([
                                         Select::make('tipo_semana')
