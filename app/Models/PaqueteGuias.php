f43a4caeb4cb7d32f89ef68dbfee49f3ed23e8d1
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-17 14:30:52 -0600

    feat: Add PaqueteGuias model and migration for paquete_guias table

---
+++ app/Models/PaqueteGuias.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaqueteGuias extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'periodo',
+        'semana',
+        'regiones_id',
+        'estado',
+        'created_at',
+        'updated_at',
+        'user_id'
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function regiones(): BelongsTo
+    {
+        return $this->belongsTo(Regiones::class, 'regiones_id');
+    }
+
+    public function guias()
+    {
+        return $this->hasMany(Guia::class);
+    }
+}
