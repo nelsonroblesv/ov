ca5b43a980249f2da8061e0e647c08f63430065a
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-17 14:53:42 -0600

    feat: Enhance PaqueteGuiasResource form with new fields and validation

--- app/Filament/Resources/PaqueteGuiasResource.php
+++ app/Filament/Resources/PaqueteGuiasResource.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PaqueteGuiasResource\Pages;
 use App\Filament\Resources\PaqueteGuiasResource\RelationManagers;
 use App\Models\PaqueteGuias;
+use App\Models\Regiones;
 use Filament\Forms;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -27,7 +33,40 @@ class PaqueteGuiasResource extends Resource
     {
         return $form
             ->schema([
-                //
+               Section::make('Informacion del Paquete')->schema([
+
+                Hidden::make('user_id')->default(fn() => auth()->id()),
+
+                TextInput::make('periodo')
+                ->required(),
+
+                Select::make('semana')
+                ->label('Semana')
+                ->options([
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                    '4' => '4',
+                ]),
+
+                Select::make('regiones_id')
+                ->label('Región')
+                ->placeholder('Seleccione una región')
+                ->required()
+                ->options(
+                    Regiones::query()
+                        ->where('is_active', true)
+                        ->pluck('name', 'id')
+                ),
+
+                Select::make('estado')
+                    ->label('Estado del Paquete')
+                    ->options([
+                        'pendiente' => 'Pendiente',
+                        'completado' => 'Completado',
+                    ])->default('pendiente')
+
+               ])->columns(2)
             ]);
     }
 
@@ -35,7 +74,16 @@ class PaqueteGuiasResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('semana')
+                    ->label('Semana'),
+                TextColumn::make('periodo')
+                    ->label('Periodo'),
+                TextColumn::make('regiones.name')
+                    ->label('Región'),
+                TextColumn::make('users.name')
+                    ->label('Registrado por'),
+                TextColumn::make('created_at')
+                    ->label('Fecha de Registro')
             ])
             ->filters([
                 //
