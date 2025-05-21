423e467c5a105a622de6c32a34932fbe44a1fcaa
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-20 18:02:07 -0600

    feat. Hide buttons from edit OrdersCursomer

--- app/Filament/Resources/CustomerOrdersResource/Pages/EditCustomerOrders.php
+++ app/Filament/Resources/CustomerOrdersResource/Pages/EditCustomerOrders.php
@@ -4,8 +4,10 @@ namespace App\Filament\Resources\CustomerOrdersResource\Pages;
 
 use App\Filament\Resources\CustomerOrdersResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Enums\IconPosition;
 
 class EditCustomerOrders extends EditRecord
 {
@@ -33,5 +35,17 @@ class EditCustomerOrders extends EditRecord
             ->icon('heroicon-o-check')
             ->iconColor('success')
             ->color('success');
+    }
+
+    // Hide the "Save" button
+    protected function getSaveFormAction(): Action
+    {
+        return parent::getSaveFormAction()->hidden(true);
+    }
+
+    // Hide the "Cancel" button
+    protected function getCancelFormAction(): Action
+    {
+        return parent::getCancelFormAction()->hidden(true);
     }
 }
