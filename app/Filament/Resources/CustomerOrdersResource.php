697b3d127b78d71d0750b1e60e063a473872886a
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-23 10:22:57 -0600

    feat. Create Payment in CustomerOrder. Add Notification.

--- app/Filament/Resources/CustomerOrdersResource.php
+++ app/Filament/Resources/CustomerOrdersResource.php
@@ -7,14 +7,24 @@ use App\Filament\Resources\CustomerOrdersResource\RelationManagers;
 use App\Filament\Resources\CustomerOrdersResource\RelationManagers\OrdersRelationManager;
 use App\Models\Customer;
 use App\Models\CustomerOrders;
+use App\Models\Payments;
+use App\Models\User;
+use Carbon\Carbon;
+use Doctrine\DBAL\Query\QueryException;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\ActionsPosition;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -109,16 +119,93 @@ class CustomerOrdersResource extends Resource
                 Tables\Actions\EditAction::make()
                     ->label('Detalles')
                     ->color('info'),
+
                 Tables\Actions\Action::make('registrar-pago')
                     ->label('Registrar Pago')
                     ->color('success')
-                    ->icon('heroicon-o-banknotes'),
+                    ->icon('heroicon-o-banknotes')
+                    ->form([
+                        Section::make('Informacion del Pago')->schema([
+
+                            Select::make('user_id')
+                                ->required()
+                                ->label('Registrado por:')
+                                ->options(
+                                    fn() =>
+                                    User::whereIn('id', function ($query) {
+                                        $query->select('id')
+                                            ->from('users')
+                                            ->where('is_active', true)
+                                            ->where('role', 'Vendedor')
+                                            ->orWhere('username', 'OArrocha')
+                                            ->orderBy('name', 'DESC');
+                                    })->pluck('name', 'id')
+                                ),
+
+                            TextInput::make('importe')
+                                ->required()
+                                ->numeric()
+                                ->minValue(1)
+                                ->placeholder('0.00'),
+
+                            Select::make('tipo')
+                                ->options([
+                                    'E' => 'Efectivo',
+                                    'T' => 'Transferencia',
+                                    'O' => 'Otro',
+                                ])
+                                ->default('E')
+                                ->required(),
+
+                            DatePicker::make('created_at')
+                                ->label('Fecha de pago')
+                                ->default(Carbon::now())
+                                ->required(),
+
+                            FileUpload::make('voucher')
+                                ->label('Comprobante')
+                                ->directory('recibos')
+                                ->columnSpanFull(),
+
+                            Textarea::make('notas')
+                                ->rows(3)
+                                ->columnSpanFull(),
+                        ])->columns(2)
+
+                    ])
+                    ->modalHeading('Registrar Pago')
+                    ->modalDescription('Registrar un nuevo pago del cliente.')
+                    ->modalSubmitActionLabel('Registrar Pago')
+                    ->action(function (array $data, Customer $record): void {
+                        // Manejo de errores
+                        try {
+                           Payments::insert([
+                                'customer_id' =>  $record->id,
+                                'user_id'  => $data['user_id'],
+                                'importe' => $data['importe'],
+                                'tipo' => $data['tipo'],
+                                'voucher'   => $data['voucher'],
+                                'notas'     => $data['notas'],
+                                'is_verified'     => true,
+                                'created_at'     => $data['created_at'],
+                            ]);
+                        } catch (QueryException $e) {
+                            Notification::make()
+                                ->danger()
+                                ->title('Error al registrar el Pago.')
+                                ->body($e->getMessage())
+                                ->send();
+                            return;
+                        }
+                        Notification::make()
+                            ->success()
+                            ->title('Pago registrado.')
+                            ->body('Se ha registrado un Pago por '.number_format($data['importe'],2).' al cliente '.$record->name .' de manera exitosa con fecha '.$data['created_at'].'.')
+                            ->icon('heroicon-o-banknotes')
+                            ->send();
+                    }),
             ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    //  Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->bulkActions([]);
     }
 
     public static function getRelations(): array
