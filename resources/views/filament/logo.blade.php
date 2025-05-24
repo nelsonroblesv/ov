6079852f929717bb128cbbf2138ddfbea91f6d81
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-24 13:43:53 -0600

    feat. Change logo PDF

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="flex items-center space-x-4">
-    <img src="{{ asset('images/logo_ovalleB.png') }}" alt="OValle" width="45" style="">   
+    <img src="{{ asset('images/logo_ovalleB.png') }}" alt="Logo OV" width="45" style="">   
 </div>
