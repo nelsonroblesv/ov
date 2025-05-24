f65b93902f4019a930d689c2e69535e9ed000f41
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-24 08:11:17 -0600

    feat. OV LOGO invoice

--- resources/views/reusable/invoice/customer_invoice.blade.php
+++ resources/views/reusable/invoice/customer_invoice.blade.php
@@ -12,7 +12,7 @@
                     <p style="color: #a0aec0;">Fecha: {{ now()->format('d/m/Y') }}</p>
                 </td>
                 <td style="text-align: right;">
-                    <img src="https://app.osberthvalle.com/images/logo_ovalleB.png" alt="Company Logo" style="width: 64px;">
+                    <img src="https://app.osberthvalle.com/images/logo_ovalleB.png" alt="OV LOGO" style="width: 64px;">
                 </td>
             </tr>
         </table>
