3048f338b8f4535948f596f450f7292c3512dfcc
Nelson Robles <nelsonroblesv@gmail.com>
2025-05-30 12:00:11 -0600

    feat. Table design widget user panel

--- app/Filament/App/Widgets/MisVisitas.php
+++ app/Filament/App/Widgets/MisVisitas.php
@@ -3,6 +3,7 @@
 namespace App\Filament\App\Widgets;
 
 use App\Filament\App\Resources\CustomerStatementResource;
+use App\Filament\Resources\CustomerResource;
 use Filament\Tables;
 use App\Models\EntregaCobranzaDetalle;
 use Carbon\Carbon;
@@ -18,99 +19,42 @@ Carbon::setLocale('es');
 
 class MisVisitas extends BaseWidget
 {
+
+    //protected int | string | array $columnSpan = 'full';
+
     public function table(Table $table): Table
     {
-         return $table
-            ->heading('Visitas para hoy: ' . Carbon::now()->isoFormat('dddd, D [de] MMMM, YYYY'))
+        return $table
+            ->heading('Visitas para hoy: ' . Carbon::now()->isoFormat('dddd D [de] MMMM, YYYY'))
             ->query(
                 EntregaCobranzaDetalle::query()
                     ->where('user_id', Auth::id())
+                    ->whereDate('fecha_programada', '<=', Carbon::now())
+                    ->where('status', false)
                     ->with('customer', 'entregaCobranza')
             )
             ->columns([
-                TextColumn::make('fecha_programada')
-                    ->label('Fecha')
-                    ->sortable()
-                    ->date(),
 
-                TextColumn::make('customer.regiones.name')
-                    ->label('Region')
-                    ->sortable()
-                    ->badge()
-                    ->color('info'),
 
                 TextColumn::make('customer.zona.nombre_zona')
-                    ->label('Zona')
-                    ->sortable()
-                    ->badge()
-                    ->color('danger'),
+                    ->label('Ubicación')
+                    ->color('info')
+                    ->description(fn($record) => $record->customer?->regiones?->name ?? 'Sin información'),
 
-                TextColumn::make('customer.name')->label('Cliente'),
                 TextColumn::make('tipo_visita')
-                    ->label('Tipo')
+                    ->label('Visita')
                     ->sortable()
-                    ->badge()
-                    ->formatStateUsing(fn(string $state): string => [
-                        'PR' => 'Prospecto',
-                        'PO' => 'Posible',
-                        'EP' => 'Entrega Primer Pedido',
-                        'ER' => 'Entrega Recurrente',
-                        'CO' => 'Cobranza',
-                    ][$state] ?? 'Otro')
-                    ->colors([
-                        'danger' => 'PR',
-                        'warning' => 'PO',
-                        'info' => 'EP',
-                        'success' => 'ER',
-                        'primary' => 'CO',
-                    ]),
-
-                IconColumn::make('status')
-                    ->label('Estatus')
-                    ->sortable()
-                    ->boolean()
-                    ->alignCenter(),
-
-                TextColumn::make('fecha_visita')->label('Visita')->date()
-                    ->toggleable(isToggledHiddenByDefault:true)
-                    ->alignCenter(),
-
-               IconColumn::make('is_verified')
-                    ->label('Verificado')
-                    ->sortable()
-                    ->boolean()
-                    ->alignCenter(),
-
-                TextColumn::make('fecha_visita')->label('Visita')->date()
-                    ->alignCenter(),
+                    ->html() // permite HTML (importante para <br>)
+                    ->formatStateUsing(function ($record) {
+                        $customer = $record->customer?->name ?? 'Sin información';
+                        $phone = $record->customer?->phone ?? 'Sin información';
+                        return "
+                            <span class='text-blue-600 font-semibold'>{$customer}</span><br>
+                            <span class='text-red-600'>{$phone}</span>";
+                    })
+                    //->description(fn($record) => $record->customer?->name ?? 'Sin información')
             ])
-            ->filters([
-                Filter::make('hoy')
-                    ->label('Hoy')
-                    ->query(function (Builder $query) {
-                        $query->whereHas('entregaCobranza', function ($q) {
-                            $q->whereDate('fecha_programada', Carbon::now());
-                        });
-                    }),
-
-                Filter::make('esta_semana')
-                    ->label('Esta semana')
-                    ->query(function (Builder $query) {
-                        $query->whereHas('entregaCobranza', function ($q) {
-                            $q->whereBetween('fecha_programada', [
-                                now()->startOfWeek(),
-                                now()->endOfWeek()
-                            ]);
-                        });
-                    }),
-            ])
-            ->actions([
-                Tables\Actions\Action::make('view_invoice')
-                    ->label('Estado de Cuenta')
-                    ->icon('heroicon-o-document-chart-bar')
-                    ->url(fn($record) => CustomerStatementResource::getUrl(name: 'invoice', parameters: ['record' => $record->customer]))
-
-                    ->openUrlInNewTab()
-            ]);
+            ->filters([])
+            ->actions([]);
     }
 }
