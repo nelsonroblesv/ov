636a799034cfa063a686f0e7fe8dd691920196e2
Nelson Robles <nelsonroblesv@gmail.com>
2025-10-19 08:46:33 -0600

    feat. Add new logo images

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="flex items-center space-x-4">
-    <img src="{{ asset('images/logo_VAMA_B.png') }}" alt="Logo OV" width="45" style="">   
+    <img src="{{ asset('images/logo_VAMA_N.png') }}" alt="Logo OV" width="45" style="">   
 </div>
