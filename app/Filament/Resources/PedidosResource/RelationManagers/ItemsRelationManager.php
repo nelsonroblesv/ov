1e433460daa6d165f32896574c182be03994fb25
Nelson Robles <nelsonroblesv@gmail.com>
2025-10-29 19:54:54 -0600

    feat. Delete RM Pedidos Notifications

--- app/Filament/Resources/PedidosResource/RelationManagers/ItemsRelationManager.php
+++ app/Filament/Resources/PedidosResource/RelationManagers/ItemsRelationManager.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PedidosResource\RelationManagers;
 use App\Models\PedidosItems;
 use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
@@ -29,45 +30,44 @@ class ItemsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Section::make('Detalles del Producto')
-                    ->schema([
-                        Select::make('product_id')
-                            ->relationship('product', 'name')
-                            ->label('Productos')
-                            ->placeholder('Selecciona un producto')
-                            ->preload()
-                            ->searchable()
-                            ->required()
-                            ->reactive()
-                            ->distinct()
-                            ->columnSpanFull()
-                            ->afterStateUpdated(fn($state, Set $set) =>
-                            $set('price_publico', Product::find($state)?->price_publico ?? 0))
-                            ->afterStateUpdated(fn($state, Set $set) =>
-                            $set('total_price', Product::find($state)?->price_publico ?? 0)),
+                Section::make()->schema([
+                    Select::make('product_id')
+                        ->relationship('product', 'name')
+                        ->label('Productos')
+                        ->placeholder('Selecciona un producto')
+                        ->preload()
+                        ->searchable()
+                        ->required()
+                        ->reactive()
+                        ->distinct()
+                        ->columnSpanFull()
+                        ->afterStateUpdated(fn($state, Set $set) =>
+                        $set('price_publico', Product::find($state)?->price_publico ?? 0))
+                        ->afterStateUpdated(fn($state, Set $set) =>
+                        $set('total_price', Product::find($state)?->price_publico ?? 0))
+                        ->columnSpan(2),
 
-                        Hidden::make('price_publico')
-                            ->label('Precio unitario')
-                            ->disabled()
-                            ->dehydrated(),
+                    Hidden::make('price_publico')
+                        ->label('Precio unitario')
+                        ->disabled()
+                        ->dehydrated(),
 
-                        TextInput::make('quantity')
-                            ->label('Cantidad')
-                            ->numeric()
-                            ->default(1)
-                            ->minValue(1)
-                            ->dehydrated()
-                            ->reactive()
-                            ->required()
-                            ->debounce(600)
-                            ->live(onBlur: true)
-                            ->afterStateUpdated(fn($state, Set $set, Get $get) => $set('total_price', round($state * $get('price_publico'), 2))),
+                    TextInput::make('quantity')
+                        ->label('Cantidad')
+                        ->numeric()
+                        ->default(1)
+                        ->minValue(1)
+                        ->dehydrated()
+                        ->reactive()
+                        ->required()
+                        ->debounce(600)
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn($state, Set $set, Get $get) => $set('total_price', round($state * $get('price_publico'), 2)))
+                        ->columnSpan(1),
 
-                        Hidden::make('total_price')
-                            ->dehydrated(),
-
-                    ])->columns(3),
-
+                    Hidden::make('total_price')
+                        ->dehydrated(),
+                ])->columns(3)
             ]);
     }
 
@@ -77,6 +77,8 @@ class ItemsRelationManager extends RelationManager
             ->recordTitleAttribute('id')
             ->defaultSort('created_at', 'desc')
             ->heading('Productos en el Pedido')
+            ->emptyStateHeading('No se agregaron productos al Pedido.')
+            ->emptyStateDescription('Agrega productos al pedido para visualizarlos aquí.')
             ->columns([
                 //TextColumn::make('pedido.id_nota')->label('Orden No.'),
                 TextColumn::make('product.name')->label('Producto'),
@@ -85,8 +87,8 @@ class ItemsRelationManager extends RelationManager
                 TextColumn::make('total_price'),
                 TextColumn::make('total_price')->label('Subtotal')
                     ->summarize(Sum::make()
-                    ->formatStateUsing(fn(string $state) => '$ ' . number_format($state, 2))
-                    ->label('Total del Pedido')),
+                        ->formatStateUsing(fn(string $state) => '$ ' . number_format($state, 2))
+                        ->label('Total del Pedido')),
             ])
             ->filters([
                 //
@@ -110,7 +112,19 @@ class ItemsRelationManager extends RelationManager
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->modalHeading('Eliminar producto del Pedido')
+                    ->modalDescription('¿Estás seguro de que deseas eliminar este producto del Pedido? Esta acción no se puede deshacer.')
+                    ->modalSubmitActionLabel('Eliminar')
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Producto eliminado')
+                            ->body('El producto ha sido eliminado del Pedido.')
+                            ->icon('heroicon-o-check')
+                            ->iconColor('success')
+                            ->color('success')
+                    ),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
