6c0e9c3cd7fa076804a84de311a2b693f7e9b383
Nelson Robles <nelsonroblesv@gmail.com>
2025-11-06 19:08:00 -0600

    feat. tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './app/Filament/Pages/*.php',
     ],
     theme: {
         extend: {
